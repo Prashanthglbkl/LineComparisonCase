4c0d714847822df6a02fa03626964c8848cec51a
Prashanthglbkl <prashanthglbkl@gmail.com>
2021-11-20 21:14:08 +0530

    Adding CompareToLines File

--- Line Comaparison/Line Comaparison/Program.cs
+++ Line Comaparison/Line Comaparison/Program.cs
@@ -12,42 +12,44 @@ namespace Line_Comaparison
         {
 
             Console.WriteLine("First Line");
-            Console.Write("Enter the Value x1: ");
-            double x1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Co-rdinate x1: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter the Value y1: ");
-            double y1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Co-rdinate y1: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter the Value x2: ");
-            double x2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Co-rdinate x2: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter the Value y2: ");
-            double y2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Co-rdinate y2: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
 
             Console.WriteLine("Second Line");
-            Console.Write("Enter the Value M1: ");
-            double M1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Co-rdinate M1: ");
+            double M1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter the Value N1: ");
-            double N1 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Co-rdinate N1: ");
+            double N1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter the Value M2: ");
-            double M2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Co-rdinate M2: ");
+            double M2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("Enter the Value N2: ");
-            double N2 = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Enter the Co-rdinate N2: ");
+            double N2 = Convert.ToDouble(Console.ReadLine());
 
             double pointsx = Math.Pow(2, (x2 - x1));
             double pointsy = Math.Pow(2, (y2 - y1));
             double LengthofLine1 = Math.Sqrt((pointsx + pointsy));
-            Console.WriteLine("Length of the Line is: " + LengthofLine1);
+            Console.WriteLine("Length of the First Line is: " + LengthofLine1);
 
             double pointsM = Math.Pow(2, (M2 - M1));
             double pointsN = Math.Pow(2, (N2 - N1));
             double LengthofLine2 = Math.Sqrt((pointsM + pointsN));
-            Console.WriteLine("Length of the Line is: " + LengthofLine2);
+            Console.WriteLine("Length of the Second Line is: " + LengthofLine2);
 
-            Console.WriteLine(LengthofLine1.Equals(LengthofLine2));
+            Console.WriteLine(LengthofLine1.CompareTo(LengthofLine2));
+          
+            //gives the ouput as 1, if equals then 0 and if smaller gives the ouput as -1.
             Console.ReadLine();
 
         }
