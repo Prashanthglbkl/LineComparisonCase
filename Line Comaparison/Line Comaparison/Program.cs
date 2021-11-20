f8b7e3df57dc04549302db949ca4fe3f29841a5b
Prashanthglbkl <prashanthglbkl@gmail.com>
2021-11-20 20:22:24 +0530

    Adding lenthofline File

--- Line Comaparison/Line Comaparison/Program.cs
+++ Line Comaparison/Line Comaparison/Program.cs
@@ -10,7 +10,22 @@ namespace Line_Comaparison
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to the Line Comparison Program");
+            Console.Write("Enter the Co-rdinate x1: ");
+            double x1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the Co-rdinate y1: ");
+            double y1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the Co-rdinate x2: ");
+            double x2 = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Enter the Co-rdinate y2: ");
+            double y2 = Convert.ToDouble(Console.ReadLine());
+
+            double Powofpoints1 = Math.Pow(2, (x2 - x1));
+            double Powofpoints2 = Math.Pow(2, (y2 - y1));
+            double LengthofLine = Math.Sqrt((Powofpoints1 + Powofpoints2));
+            Console.WriteLine("Length of the Line is: " + LengthofLine);
             Console.ReadLine();
         }
     }
